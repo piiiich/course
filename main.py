06d5c1b062fafbdc5ed989e349c50a54d559e169
piiiich <gaspardpichon13@gmail.com>
2023-12-08 13:28:41 +0100

    Création du module view.py et adaptation du code pour son intégration

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-from PyQt5.QtWidgets import QApplication
-from geometry import Point, Polygon
-from view import PolygonApp
+import geometry as geo
+import view
 import sys
 
 def main():
-    # Example list of points
-    points = [Point(50, 50), Point(150, 50), Point(150, 150), Point(50, 150)]
-
-    # Create a Polygon object
-    polygon = Polygon(points)
-
-    app = QApplication(sys.argv)
-    ex = PolygonApp(polygon)
-    sys.exit(app.exec_())
+    
 
 if __name__ == '__main__':
     main()
--- view.py
+++ view.py
@@ -1,7 +1,11 @@
-import geometry
+import geometry as geo
 
 def main():
-    pass
+    # Example list of points
+    points = [geo.Point(50, 50), geo.Point(150, 50), geo.Point(150, 150), geo.Point(50, 150)]
+
+    # Create a Polygon object
+    polygon = geo.Polygon(points)
 
 if __name__ == '__main__':
     main()
