c6c7c9ca7f200b5f8ed40b899f1a2b99be916132
Emma Paillart <emma.paillart@alumni.enac.fr>
2023-12-11 14:29:13 +0100

    tests trajectoires

--- trajectoires.py
+++ trajectoires.py
@@ -1,20 +1,25 @@
 import numpy as np
 
-#u = [ux, uy]
-#v = [vx, vy]
-
-def clockwise(u,v):
-    d = np.det(u, v)
-    if d < 0 :
-        return -1
-    elif d > 0 :
-        return 1
-    #elif ux*vx < 0 or uy*vy < 0 :
-    elif u[0]*v[0] < 0 or u[1]*v[1] < 0 :
-        return -1
-    #elif u.u < v.v :
-    elif np.dot(u, u) < np.dot(v, v) :
-        return 1
-    else :
-        return 0
+def clockwise(u, v):
+    '''
+    fonction de test si les segments u et v sont séquents
+    '''
+    d = np.linalg.det([u, v])
+    if d < 0 or u[0]*v[0] < 0 or u[1]*v[1] < 0 :
+        return -1 #si orientation dans le sens inverse des aiguilles d'une montre
     
+    elif d > 0 or np.dot(u, u) < np.dot(v, v):
+        return 1  #si orientation dans le sens des aiguilles d'une montre
+    
+    else :
+        return 0 #si colinéaires
+  
+def are_vectors_sequent(u, v):
+    orientation = clockwise(u, v)
+    return orientation in [1, -1, 0]
+
+# Test
+u = np.array([1, 1])
+v = np.array([0, 2])
+result = are_vectors_sequent(u, v)
+print(result)
