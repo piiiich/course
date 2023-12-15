d6f54df6514a67d583fd044fe861e0f988c58776
Juliecgnt <jujuliexbd@gmail.com>
2023-12-15 13:27:43 +0100

    rien

--- trajectoires.py
+++ trajectoires.py
@@ -1,14 +1,15 @@
+
 import numpy as np
 
 def clockwise(u, v):
     '''
     fonction de test si les segments u et v sont séquents
     '''
-    d = np.linalg.det([u, v])
+    d = numpy.linalg.det([u, v])
     if d < 0 or u[0]*v[0] < 0 or u[1]*v[1] < 0 :
         return -1 #si orientation dans le sens inverse des aiguilles d'une montre
     
-    elif d > 0 or np.dot(u, u) < np.dot(v, v):
+    elif d > 0 or numpy.dot(u, u) < numpy.dot(v, v):
         return 1  #si orientation dans le sens des aiguilles d'une montre
     
     else :
@@ -19,7 +20,8 @@ def are_vectors_sequent(u, v):
     return orientation in [1, -1, 0]
 
 # Test
-u = np.array([1, 1])
-v = np.array([0, 2])
+u = numpy.array([1, 1])
+v = numpy.array([0, 2])
 result = are_vectors_sequent(u, v)
 print(result)
+
