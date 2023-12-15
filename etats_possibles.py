692080ed7b426eb844eea0b8e453996aa511e07b
Emma Paillart <emma.paillart@alumni.enac.fr>
2023-12-15 14:25:44 +0100

    création module etats_possibles

---
+++ etats_possibles.py
@@ -0,0 +1,9 @@
+import circuit, voiture 
+
+#déterminer tous les états suivants possibles 
+#tester si on ne croise pas les bords ext et int de la section 
+#si on passe par une limite de secteur, tester les bords ext et int de ce meme secteur
+#si on passe par une deuxième limite de secteur, tester aussi 
+#ne pas pouvoir retourner au secteur précédent, ou en arrière 
+# AB.v >= 0 et CD.v >= 0
+
