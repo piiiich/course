b22e65832ef9c5bee61d23686997dcbaf6ed158f
Emma Paillart <emma.paillart@alumni.enac.fr>
2024-01-15 15:00:19 +0100

    supression commentaires

--- voiture.py
+++ voiture.py
@@ -9,44 +9,6 @@ import intersection as X
 # Largeur de la voiture
 CAR_WIDTH = 70
 
-# Tri fusion 
-'''
-def merge(left, right):
-        merged = []
-        left_index = 0
-        right_index = 0
-
-        while left_index < len(left) and right_index < len(right):
-            if left[left_index][1] >= right[right_index][1]:  # Notez l'inversion du signe pour un tri décroissant
-                merged.append(left[left_index])
-                left_index += 1
-            else:
-                merged.append(right[right_index])
-                right_index += 1
-
-        merged.extend(left[left_index:])
-        merged.extend(right[right_index:])
-
-        return merged
-
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left_half = arr[:mid]
-    right_half = arr[mid:]
-
-    return merge(merge_sort(left_half), merge_sort(right_half))
-
-
-        # liste_distances = self.liste_distances(init_pos, init_speed)
-        # Tri par fusion pour trier les points possibles par distance à l'origine décroissante
-        #liste_coups_triee = merge_sort(liste_coups) 
-        #print(f'liste_distances = {[tab[:3] for tab in liste_distances]}')
-        #return [tab[:3] for tab in liste_distances]
-'''
-
 class Voiture(QGraphicsEllipseItem):
     def __init__(self, ecurie, reach):
         super().__init__()
@@ -86,7 +48,7 @@ class Voiture(QGraphicsEllipseItem):
         return liste_coups
 
 
-    def tri_liste_distances(self, pos, speed): # init_pos, init_speed):
+    def tri_liste_distances(self, pos, speed):  
         ''' Cette fonction renvoie une liste de tuples (point_destination_(x,y), vitesse, distance) '''
         liste_coups = self.liste_distances(pos, speed)
         liste_coups_triee = sorted(liste_coups, key = lambda x : x[2])
